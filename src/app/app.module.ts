6762798d4587cb45f7975999b8a1b787334fa915
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2018-10-17 15:42:37 +0200

    Created playground component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppComponent } from './app.component';
 import { CKEditorModule } from '../ckeditor/ckeditor.module';
 import { SimpleUsageComponent } from './simple-usage/simple-usage.component';
 import { DemoFormComponent } from './demo-form/demo-form.component';
+import { PlaygroundComponent } from './playground/playground.component';
 
 const appRoutes: Routes = [
 	{ path: '', redirectTo: '/simple-usage', pathMatch: 'full' },
 	{ path: 'simple-usage', component: SimpleUsageComponent },
 	{ path: 'forms', component: DemoFormComponent },
+	{ path: 'playground', component: PlaygroundComponent }
 ];
 
 @NgModule( {
@@ -24,7 +26,8 @@ const appRoutes: Routes = [
 	declarations: [
 		AppComponent,
 		DemoFormComponent,
-		SimpleUsageComponent
+		SimpleUsageComponent,
+		PlaygroundComponent
 	],
 	providers: [],
 	bootstrap: [ AppComponent ]
---
+++ src/app/playground/playground.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+@Component( {
+	selector: 'app-playground',
+	templateUrl: './playground.component.html',
+	styleUrls: [ './playground.component.css' ]
+} )
+export class PlaygroundComponent implements OnInit {
+
+	@ViewChild( 'classic' ) classic;
+
+	@ViewChild( 'inline' ) inline;
+
+	public myModel: any;
+
+	constructor() {
+		window.component = this;
+	}
+
+	ngOnInit() {
+	}
+
+}
