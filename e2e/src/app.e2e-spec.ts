c48ae9e35f9e92377201cab45b62960cc159e8cb
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2018-10-26 17:23:08 +0200

    E2e: updated tests.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,56 @@
 import { AppPage } from './app.po';
+import { protractor, WebElement } from 'protractor';
 
-describe('workspace-project App', () => {
-  let page: AppPage;
+describe( 'workspace-project App', () => {
+	let page: AppPage,
+		editables: WebElement[];
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+	beforeEach( () => {
+		page = new AppPage;
+	} );
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to ckeditor4-angular!');
-  });
-});
+	describe( 'simple-usage', () => {
+		beforeEach( () => {
+			page.navigateTo();
+		} );
+
+		beforeEach( async () => {
+			editables = [ await page.getEditable(), await page.getInlineEditable() ];
+		} );
+
+		it( 'should display welcome message', () => {
+			expect( page.getParagraphText() ).toEqual( 'CKEditor integration with Angular 2+' );
+		} );
+
+		it( 'should display editor with initial content', async () => {
+			editables.forEach( editable => expect( page.getHtmlString( editable ) )
+				.toBe( '<p>Getting used to an entirely different culture can be challeng' +
+					'ing. While it’s also nice to learn about cultures online or from books, nothing comes close to experiencing cultural d' +
+					'iversity in person. You learn to appreciate each and every single one of the differences while you become more cultura' +
+					'lly fluid.</p>' )
+			);
+		} );
+
+		describe( 'typing', () => {
+			it( `in editor1 should update editors content`, testTyping( 0 ) );
+			it( `in editor2 should update editors content`, testTyping( 1 ) );
+
+			function testTyping( editableNumber: number ) {
+				return async function() {
+					const keys = [
+						'Foo! ',
+						protractor.Key.chord( protractor.Key.CONTROL, 'b' ),
+						'Bar?'
+					];
+
+					await page.updateValue( editables[ editableNumber ], keys );
+
+					editables.forEach( item => {
+						expect( page.getHtmlString( item ) )
+							.toBe( '<p>Foo!&nbsp;<strong>Bar?</strong></p>' );
+					} );
+				};
+			}
+		} );
+	} );
+} );
