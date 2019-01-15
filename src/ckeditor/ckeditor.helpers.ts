9040190a85d5bad9cf3649af9cc9a5ea70772558
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2019-01-15 15:56:10 +0100

    Added ckeditro.helpers.ts file with 'getEditorNameSpace' method.

---
+++ src/ckeditor/ckeditor.helpers.ts
@@ -0,0 +1,32 @@
+/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+import loadScript from 'load-script';
+
+declare let CKEDITOR: any;
+let promise;
+
+export function getEditorNamespace( editorURL: string ): Promise<object> {
+	if ( typeof editorURL !== 'string' || editorURL.length < 1 ) {
+		throw new TypeError( 'CKEditor URL must be a non-empty string.' );
+	}
+
+	if ( 'CKEDITOR' in window ) {
+		return Promise.resolve( CKEDITOR );
+	} else if ( !promise ) {
+		promise = new Promise( ( scriptResolve, scriptReject ) => {
+			loadScript( editorURL, err => {
+				if ( err ) {
+					scriptReject( err );
+				} else {
+					scriptResolve( CKEDITOR );
+					promise = undefined;
+				}
+			} );
+		} );
+	}
+
+	return promise;
+}
