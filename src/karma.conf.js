ddd026a18a676e7ff31958d00a2b98ccd770e629
Jacek Bogdanski <jacekbogd@gmail.com>
2019-05-06 13:49:34 +0200

    Added development CKEditor version.

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -12,6 +12,12 @@ module.exports = function (config) {
 			require('karma-coverage-istanbul-reporter'),
 			require('@angular-devkit/build-angular/plugins/karma')
 		],
+		files: [
+			{ pattern: '../node_modules/ckeditor/ckeditor.js', included: true, served: true, watched: false, nocache: true },
+			{ pattern: '../node_modules/ckeditor/+(core|plugins|skins|lang)/**/*', included: false, served: true, watched: false, nocache: true },
+			{ pattern: '../node_modules/ckeditor/+(config|styles).js', included: false, served: true, watched: false, nocache: true },
+			{ pattern: '../node_modules/ckeditor/contents.css', included: false, served: true, watched: false, nocache: true },
+		],
 		client: {
 			clearContext: false // leave Jasmine Spec Runner output visible in browser
 		},
