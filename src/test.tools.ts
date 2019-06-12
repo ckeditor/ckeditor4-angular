9162c5dd10a76565249d78b464c68ff6e360b18d
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2019-06-12 11:58:45 +0200

    Replace default export class, with single export function.

--- src/test.tools.ts
+++ src/test.tools.ts
@@ -1,9 +1,7 @@
 import { CKEditorComponent } from './ckeditor/ckeditor.component';
 
-export class TestTools {
-	static whenEvent( evtName: string, component: CKEditorComponent ) {
-		return new Promise( res => {
-			component[ evtName ].subscribe( res );
-		} );
-	}
+export function whenEvent( evtName: string, component: CKEditorComponent ) {
+	return new Promise( res => {
+		component[ evtName ].subscribe( res );
+	} );
 }
