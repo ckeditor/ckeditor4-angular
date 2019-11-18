153d8b779a89f3b7be11ba23926530d39c536902
Dariusz Jarzębski <d.jarzebski@cksource.com>
2019-11-18 12:56:20 +0100

    Add polyfills for IE11.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -25,7 +25,22 @@
 
 /***************************************************************************************************
  * Browser Polyfills
+ * IE11 needs those in general. Currently we use only some of them in tests using console and ensuring that 'divarea' plugin is loaded,
+ * but they may come in handy later.
  */
+
+import 'core-js/es/symbol';
+import 'core-js/es/object';
+import 'core-js/es/function';
+import 'core-js/es/parse-int';
+import 'core-js/es/parse-float';
+import 'core-js/es/number';
+import 'core-js/es/math';
+import 'core-js/es/string';
+import 'core-js/es/date';
+import 'core-js/es/array';
+import 'core-js/es/regexp';
+import 'core-js/es/map';
 
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
