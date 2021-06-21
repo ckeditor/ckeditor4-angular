d66428766c67d918467f911fa46026f81e86e90f
Dariusz Jarzębski <d.jarzebski@cksource.com>
2021-06-21 16:16:36 +0200

    Add license banners.

--- scripts/_helpers/logger.js
+++ scripts/_helpers/logger.js
@@ -1,3 +1,10 @@
+/**
+ * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+'use strict';
+
 const chalk = require( 'chalk' );
 
 module.exports = class Logger {
