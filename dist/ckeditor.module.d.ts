76e5de4dfade4435d4e0a8f9b98fbbd0b8c687bf
Jacek Bogdanski <jacekbogd@gmail.com>
2022-01-12 10:22:08 +0100

    Clean after release [ci skip]

--- dist/ckeditor.module.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md.
- */
-export declare class CKEditorModule {
-}
-export * from './ckeditor';
-export { CKEditorComponent } from './ckeditor.component';
