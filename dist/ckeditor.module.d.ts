2375808239b785e7ea722c673479c0e3f2e75fd5
Jacek Bogdanski <jacekbogd@gmail.com>
2022-07-20 14:53:27 +0200

    3.1.1

---
+++ dist/ckeditor.module.d.ts
@@ -0,0 +1,8 @@
+/**
+ * @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+export declare class CKEditorModule {
+}
+export * from './ckeditor';
+export { CKEditorComponent } from './ckeditor.component';
