d7724117a51c5ef5b7e13a7543145146fee285cb
Jacek Bogdanski <j.bogdanski@cksource.com>
2025-02-05 15:55:34 +0100

    Updated default CDN CKEditor 4 dependency to 4.25.1

--- src/ckeditor/ckeditor.component.ts
+++ src/ckeditor/ckeditor.component.ts
@@ -49,9 +49,9 @@ export class CKEditorComponent implements AfterViewInit, OnDestroy, ControlValue
 	/**
 	 * CKEditor 4 script url address. Script will be loaded only if CKEDITOR namespace is missing.
 	 *
-	 * Defaults to 'https://cdn.ckeditor.com/4.25.0-lts/standard-all/ckeditor.js'
+	 * Defaults to 'https://cdn.ckeditor.com/4.25.1-lts/standard-all/ckeditor.js'
 	 */
-	@Input() editorUrl = 'https://cdn.ckeditor.com/4.25.0-lts/standard-all/ckeditor.js';
+	@Input() editorUrl = 'https://cdn.ckeditor.com/4.25.1-lts/standard-all/ckeditor.js';
 
 	/**
 	 * Tag name of the editor component.
